12d4503b66e36e257dabc79e06955df98b58d002
TZbooo <helloworldbooo@gmail.com>
2023-06-09 15:38:39 +0300

    delete telegram scraper

--- scraper/config.py
+++ scraper/config.py
@@ -15,10 +15,6 @@ with open(BASE_DIR / 'scraping.json', 'r') as scraping_conf_file:
 
 KYC_BASE_API_TOKEN = os.environ['KYC_BASE_API_TOKEN']
 
-MTPROTO_TOKEN = os.environ['MTPROTO_TOKEN']
-MTPROTO_API_HASH = os.environ['MTPROTO_API_HASH']
-MTPROTO_API_ID = os.environ['MTPROTO_API_ID']
-
 CELERY_BROKER_URL = os.environ['CELERY_BROKER_URL']
 CELERY_RESULT_BACKEND = os.environ['CELERY_RESULT_BACKEND']
 
--- scraper/worker.py
+++ scraper/worker.py
@@ -11,7 +11,6 @@ celery.control.purge()
 celery.conf.broker_url = CELERY_BROKER_URL
 celery.conf.result_backend = CELERY_RESULT_BACKEND
 celery.autodiscover_tasks([
-    'scraper.telegram_channel',
     'scraper.moscow_post',
     'scraper.lenta_ru',
     'scraper.washington_post',
